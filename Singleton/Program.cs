f2761e45bac92bf89ebe397fc54ee77417d4c0f0
dp140719 <52877500+dp140719@users.noreply.github.com>
2019-07-14 11:17:40 +0300

    Add files via upload

---
+++ Singleton/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Singleton s1 = Singleton.GetInstance();
+            Singleton s2 = Singleton.GetInstance();
+            Singleton s3 = Singleton.GetInstance();
+            Singleton s4 = Singleton.GetInstance();
+
+            if(s1 == s4)
+            {
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Singleton
+    {
+
+        private static Singleton INSTANCE;
+
+        private static object key = new object();
+        private Singleton()
+        {
+
+        }
+
+        public static Singleton GetInstance()
+        {
+            if (INSTANCE != null)
+            {
+                return INSTANCE;
+            }
+
+            lock (key)
+            {
+                if (INSTANCE == null)
+                {
+                    INSTANCE = new Singleton();
+                }
+            }
+
+            return INSTANCE;
+        }
+
+    }
+}
